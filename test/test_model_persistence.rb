891027a873c0fa5336a348e413decfb2718b9f89
msalvadores <msalvadores@gmail.com>
2012-11-11 15:08:25 -0500

    unit test that updates a date field.

--- test/test_model_persistence.rb
+++ test/test_model_persistence.rb
@@ -43,6 +43,31 @@ class TestModelPersonB < TestCase
     person_update.delete
     assert_equal false, person_update.exists?(reload=true)
   end
+
+  def test_person_update_date
+    person = Person.new({:name => "Goo Fernandez",
+                        :birth_date => DateTime.parse("2012-10-04T07:00:00.000Z"),
+                        :some_stuff => [1]})
+    if person.exists?
+      person_copy = Person.new
+      person_copy.load(person.resource_id)
+      person_copy.delete
+    end
+    assert_equal false, person.exists?(reload=true)
+    person.save
+    person_update = Person.new()
+    person_update.load(person.resource_id)
+    person_update.birth_date =  DateTime.parse("2013-01-01T07:00:00.000Z")
+    person_update.save
+    assert_equal DateTime.parse("2013-01-01T07:00:00.000Z"), person_update.birth_date
+    #reload
+    person_update = Person.new()
+    person_update.load(person.resource_id)
+    assert_equal DateTime.parse("2013-01-01T07:00:00.000Z"), person_update.birth_date
+    assert_equal "Goo Fernandez", person_update.name
+    person_update.delete
+    assert_equal false, person_update.exists?(reload=true)
+  end
   
 end
 
