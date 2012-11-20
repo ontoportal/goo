a76f4d2464bf390e98824d1250810ac4530063b9
msalvadores <msalvadores@gmail.com>
2012-11-20 12:44:55 -0800

    Tests for updating dependent objects.

--- test/test_model_dependent.rb
+++ test/test_model_dependent.rb
@@ -35,7 +35,7 @@ class TestModelDependencies < TestCase
     end
   end
 
-  def test_model_deps_save
+  def _model_deps_save
     m1 = Model1.new
     m1.prop = "m1"
     if m1.exists?
@@ -77,5 +77,48 @@ class TestModelDependencies < TestCase
     assert_equal false, m2.exists?(reload=true)
     assert_equal false, m2.persistent?
   end
+
+
+  def test_model_deps_update
+    m1 = Model1.new
+    m1.prop = "m1"
+    if m1.exists?
+      m1_copy = Model1.new
+      m1_copy.load(m1.resource_id)
+      m1_copy.delete
+    end
+    m2 = Model2.new
+    m2.prop = "m2"
+    if m2.exists?
+      m2_copy = Model2.new
+      m2_copy.load(m2.resource_id)
+      m2_copy.delete
+    end
+    assert_equal false, m1.exists?(reload=true)
+    assert_equal false, m2.exists?(reload=true)
+    m1.m2 = m2
+    assert_instance_of Array, m1.m2
+    assert_instance_of Model2, m1.m2[0]
+    assert_equal "m2", m1.m2[0].prop
+
+    m1.save
+    #modify m2 save m1
+    m1.m2[0].new_prop = "m2 new"
+    assert_equal true, m1.modified?
+    m1.save
+    assert_equal 1, count_pattern("#{m1.resource_id.to_turtle} a ?type .")
+    assert_equal 1, count_pattern("#{m1.m2[0].resource_id.to_turtle} a ?type .")
+    m1.delete
+    assert_equal false, m1.exists?(reload=true)
+    assert_equal true, m2.exists?(reload=true)
+    assert_equal 0, count_pattern("#{m1.resource_id.to_turtle} a ?type .")
+    assert_equal 1, count_pattern("#{m2.resource_id.to_turtle} a ?type .")
+    m2.delete
+    assert_equal false, m1.exists?(reload=true)
+    assert_equal false, m2.exists?(reload=true)
+    assert_equal 0, count_pattern("#{m1.resource_id.to_turtle} a ?type .")
+    assert_equal 0, count_pattern("#{m2.resource_id.to_turtle} a ?type .")
+  end
+
 end
 
