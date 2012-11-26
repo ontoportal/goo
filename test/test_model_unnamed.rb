37ee179e293517fdb1c7a2af14f24cdeb6e2e09c
msalvadores <msalvadores@gmail.com>
2012-11-26 12:14:48 -0800

    Test for named object that depends on an unnamed.

--- test/test_model_unnamed.rb
+++ test/test_model_unnamed.rb
@@ -1,5 +1,17 @@
 require_relative 'test_case'
 
+class Named < Goo::Base::Resource
+  model :named
+  validates :name, :presence => true, :cardinality => { :maximum => 1 }
+  validates :hasUnnammed, :instance_of => { :with => :unnamed } 
+
+  unique :name
+
+  def initialize(attributes = {})
+    super(attributes)
+  end
+end
+
 class Unnamed < Goo::Base::Resource
   model :unnamed
   validates :name, :presence => true, :cardinality => { :maximum => 1 }
@@ -14,10 +26,12 @@ class TestModelUnnamed < TestCase
   def initialize(*args)
     super(*args)
     voc = Goo::Naming.get_vocabularies
-    if not voc.is_type_registered? :with_no_name
-      voc.register_model(:foaf, :with_no_name, Unnamed)
+    if not voc.is_type_registered? :unnamed
+      voc.register_model(:foaf, :unnamed, Unnamed)
+      voc.register_model(:foaf, :named, Named)
     else
-      raise StandarError, "Error conf unit test" if :with_no_name != voc.get_model_registry(Unnamed)[:type]
+      raise StandarError, "Error conf unit test" if :unnamed != voc.get_model_registry(Unnamed)[:type]
+      raise StandarError, "Error conf unit test" if :named != voc.get_model_registry(Named)[:type]
     end
   end
 
@@ -40,5 +54,26 @@ class TestModelUnnamed < TestCase
     assert_equal obj.resource_id.value, load_obj.resource_id.value
     load_obj.delete
     assert_equal false, load_obj.exist?(reload=true)
+  end
+
+  def test_named_depends_on_unnnamed
+    unn = Unnamed.new
+    unn.name = "some value"
+    unn.prop1 = 1
+
+    named = Named.new({:name => "some other value", :hasUnnammed => [unn]})
+    if named.exist?
+      item = Goo::Base::Resource.load(named.resource_id)
+      item.delete
+      assert_equal false, item.exist?(reload=true)
+    end
+    named.save
+    assert_equal true, named.exist?(reload=true)
+    assert_equal 1, count_pattern("#{named.resource_id.to_turtle} a ?type .")
+    assert_equal 1, count_pattern("#{unn.resource_id.to_turtle} a ?type .")
+    named.delete
+    assert_equal false, named.exist?(reload=true)
+    assert_equal 0, count_pattern("#{named.resource_id.to_turtle} a ?type .")
+    assert_equal 0, count_pattern("#{unn.resource_id.to_turtle} a ?type .")
   end
 end
