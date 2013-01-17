9be163db5526e9c6a3dd5e6973c86d469215528c
msalvadores <msalvadores@gmail.com>
2013-01-17 11:01:04 -0800

    sanity check configure exception in a separate method.

--- lib/goo.rb
+++ lib/goo.rb
@@ -23,23 +23,28 @@ module Goo
     @@_models
   end
 
+  def self.configure_sanity_check(conf)
+    unless conf.has_key? :namespaces
+      raise ArgumentError, "Namespaces needs to be provided."
+    end
+    unless conf[:namespaces].has_key? :default
+      raise ArgumentError, "Default namespaces needs to be provided."
+    end
+    unless conf[:namespaces][:default].kind_of? Symbol and\
+           conf[:namespaces].has_key? conf[:namespaces][:default]
+      raise ArgumentError, "Default namespace must be a symbol pointing to other ns."
+    end
+    raise ArgumentError, "Store configuration not found in configuration" \
+      unless conf.has_key? :stores
+  end
+
   def self.configure
     raise ArgumentError, "Configuration needs to receive a code block" \
       if not block_given?
 
     yield @@_configuration
-    unless @@_configuration.has_key? :namespaces
-      raise ArgumentError, "Namespaces needs to be provided."
-    end
-    unless @@_configuration[:namespaces].has_key? :default
-      raise ArgumentError, "Default namespaces needs to be provided."
-    end
-    unless @@_configuration[:namespaces][:default].kind_of? Symbol and\
-           @@_configuration[:namespaces].has_key? @@_configuration[:namespaces][:default]
-      raise ArgumentError, "Default namespace must be a symbol pointing to other ns."
-    end
-    raise ArgumentError, "Store configuration not found in configuration" \
-      unless @@_configuration.has_key? :stores
+
+    configure_sanity_check(@@_configuration)
     stores = @@_configuration[:stores]
     stores.each do |store|
       SparqlRd::Repository.configuration(store)
@@ -47,8 +52,10 @@ module Goo
         @@_default_store  = SparqlRd::Repository.endpoint(store[:name])
       end
     end
+
     @@_default_store = SparqlRd::Repository.endpoint(stores[0][:name]) \
       if @@_default_store.nil?
+
     @@_uuid_generator = UUID.new
     @@_support_skolem = Goo::Naming::Skolem.detect
   end
@@ -80,7 +87,7 @@ module Goo
     return @@_configuration[:namespaces]
   end
 
-  def first_or_empty_if_nil(x)
+  def self.first_or_empty_if_nil(x)
     return x[0] if x.length > 0
     return nil
   end
