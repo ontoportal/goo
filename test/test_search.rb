b7d1de316607f12c85e3352ef10d1d92a9e28788
msalvadores <msalvadores@gmail.com>
2013-03-12 19:27:27 -0700

    change name Term for TermSearch to resolve conflict name

--- test/test_search.rb
+++ test/test_search.rb
@@ -21,8 +21,8 @@ def get_doc(res)
   return doc
 end
 
-class Term < Goo::Base::Resource
-  model :term
+class TermSearch < Goo::Base::Resource
+  model :term_search
   attribute :id, :single_value => true
   attribute :prefLabel, :not_nil => true, :single_value => true
   attribute :synonym  #array of strings
@@ -46,7 +46,7 @@ class TestModelSearch < TestCase
 
   def setup
     @terms = [
-      Term.new(
+      TermSearch.new(
         :id => "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#Melanoma",
         :prefLabel => "Melanoma",
         :synonym => ["Cutaneous Melanoma", "Skin Cancer", "Malignant Melanoma"],
@@ -56,7 +56,7 @@ class TestModelSearch < TestCase
         :semanticType => "Neoplastic Process",
         :umlsCui => "C0025202"
       ),
-      Term.new(
+      TermSearch.new(
           :id => "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#Neoplasm",
           :prefLabel => "Neoplasm",
           :synonym => ["tumor", "Neoplasms", "NEOPLASMS BENIGN", "MALIGNANT AND UNSPECIFIED (INCL CYSTS AND POLYPS)", "Neoplasia", "Neoplastic Growth"],
@@ -77,76 +77,76 @@ class TestModelSearch < TestCase
   end
 
   def test_search
-    Term.indexClear()
+    TermSearch.indexClear()
     @terms[1].index()
-    Term.indexCommit()
-    resp = Term.search(@terms[1].prefLabel)
+    TermSearch.indexCommit()
+    resp = TermSearch.search(@terms[1].prefLabel)
     assert_equal(1, resp["response"]["docs"].length)
     assert_equal @terms[1].prefLabel, resp["response"]["docs"][0]["prefLabel"]
   end
 
   def test_unindex
-    Term.indexClear()
+    TermSearch.indexClear()
     @terms[1].index()
-    Term.indexCommit()
-    resp = Term.search(@terms[1].prefLabel)
+    TermSearch.indexCommit()
+    resp = TermSearch.search(@terms[1].prefLabel)
     assert_equal(1, resp["response"]["docs"].length)
 
     @terms[1].unindex()
-    Term.indexCommit()
-    resp = Term.search(@terms[1].prefLabel)
+    TermSearch.indexCommit()
+    resp = TermSearch.search(@terms[1].prefLabel)
     assert_equal(0, resp["response"]["docs"].length)
   end
 
   def test_unindexByQuery
-    Term.indexClear()
+    TermSearch.indexClear()
     @terms[1].index()
-    Term.indexCommit()
-    resp = Term.search(@terms[1].prefLabel)
+    TermSearch.indexCommit()
+    resp = TermSearch.search(@terms[1].prefLabel)
     assert_equal(1, resp["response"]["docs"].length)
 
     query = "submissionAcronym:" + @terms[1].submissionAcronym
-    Term.unindexByQuery(query)
-    Term.indexCommit()
+    TermSearch.unindexByQuery(query)
+    TermSearch.indexCommit()
 
-    resp = Term.search(@terms[1].prefLabel)
+    resp = TermSearch.search(@terms[1].prefLabel)
     assert_equal(0, resp["response"]["docs"].length)
   end
 
   def test_index
-    Term.indexClear()
+    TermSearch.indexClear()
     @terms[0].index()
-    Term.indexCommit()
-    resp = Term.search(@terms[0].prefLabel)
+    TermSearch.indexCommit()
+    resp = TermSearch.search(@terms[0].prefLabel)
     assert_equal(1, resp["response"]["docs"].length)
     assert_equal @terms[0].prefLabel, resp["response"]["docs"][0]["prefLabel"]
   end
 
   def test_indexBatch
-    Term.indexClear()
-    Term.indexBatch(@terms)
-    Term.indexCommit()
-    resp = Term.search("*:*")
+    TermSearch.indexClear()
+    TermSearch.indexBatch(@terms)
+    TermSearch.indexCommit()
+    resp = TermSearch.search("*:*")
     assert_equal(2, resp["response"]["docs"].length)
   end
 
   def test_unindexBatch
-    Term.indexClear()
-    Term.indexBatch(@terms)
-    Term.indexCommit()
-    resp = Term.search("*:*")
+    TermSearch.indexClear()
+    TermSearch.indexBatch(@terms)
+    TermSearch.indexCommit()
+    resp = TermSearch.search("*:*")
     assert_equal(2, resp["response"]["docs"].length)
 
-    Term.unindexBatch(@terms)
-    Term.indexCommit()
-    resp = Term.search("*:*")
+    TermSearch.unindexBatch(@terms)
+    TermSearch.indexCommit()
+    resp = TermSearch.search("*:*")
     assert_equal(0, resp["response"]["docs"].length)
   end
 
   def test_indexClear
-    Term.indexClear()
-    Term.indexCommit()
-    resp = Term.search("*:*")
+    TermSearch.indexClear()
+    TermSearch.indexCommit()
+    resp = TermSearch.search("*:*")
     assert_equal(0, resp["response"]["docs"].length)
   end
 end
