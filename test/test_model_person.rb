27064506feed78eb801b1f9d1e1c2709d8c7c364
msalvadores <msalvadores@gmail.com>
2013-03-19 17:11:11 -0700

    tests for read only objects

--- test/test_model_person.rb
+++ test/test_model_person.rb
@@ -239,4 +239,23 @@ class TestModelPersonA < TestCase
     assert(!person.errors[:email].nil?)
     assert_equal(7, person.errors[:email].length)
   end
+
+  def test_read_only
+    data = { name: "some name",
+             multiple_vals: ["some mult vals", 100],
+             birth_date: DateTime.now.xmlschema
+    }
+    read_only = Goo::Base::Resource.read_only(Person, "http://example.org", data)
+    assert read_only.resource_id.kind_of? SparqlRd::Resultset::IRI
+    assert read_only.resource_id.value == "http://example.org"
+    assert_raise Exception do
+      read_only.save
+    end
+    assert_raise Exception do
+      read_only.delete
+    end
+    assert read_only.name.kind_of? SparqlRd::Resultset::StringLiteral
+    assert read_only.attributes[:name].kind_of? SparqlRd::Resultset::StringLiteral
+    assert read_only.attributes[:name] == "some name"
+  end
 end
