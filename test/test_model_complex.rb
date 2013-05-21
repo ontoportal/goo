22cba70c0db69af2f0a20aba47d226605a945509
msalvadores <msalvadores@gmail.com>
2013-05-21 15:56:33 -0700

    test complex inverse children

--- test/test_model_complex.rb
+++ test/test_model_complex.rb
@@ -21,7 +21,7 @@ class Term < Goo::Base::Resource
   attribute :definition, namespace: :skos, enforce: [:list]
   attribute :deprecated, namespace: :owl
 
-  attribute :parents, namespace: :rdfs, property: :subClassOf, enforce: [:list]
+  attribute :parents, namespace: :rdfs, property: :subClassOf, enforce: [:list, :class]
   attribute :children, namespace: :rdfs, property: :subClassOf, inverse: { on: :class , attribute: :parents }
 end
 
@@ -166,17 +166,17 @@ class TestModelComplex < MiniTest::Unit::TestCase
     unless submission.exist?
       submission.save
     else
-      submission = Submission.find("submission1")
+      submission = Submission.find("submission1").first
     end
 
-    terms = Term.where submission: submission
+    terms = Term.in(submission)
     terms.each do |t|
       t.delete
-      assert_equal 0, count_pattern("GRAPH <#{t.resource_id.value}> { #{t.resource_id.to_turtle} ?p ?o . }")
+      assert_equal 0, GooTest.count_pattern("GRAPH #{t.id.to_ntriples} { #{t.id.to_ntriples} ?p ?o . }")
     end
 
     vehicle = Term.new
-    vehicle.resource_id = RDF::URI.new "http://someiri.org/vehicle"
+    vehicle.id = RDF::URI.new "http://someiri.org/vehicle"
     vehicle.submission = submission
     vehicle.prefLabel = "vehicle"
     vehicle.synonym = ["transport", "vehicles"]
@@ -186,39 +186,40 @@ class TestModelComplex < MiniTest::Unit::TestCase
 
     van = Term.new
     van.submission = submission
-    van.resource_id = RDF::URI.new "http://someiri.org/van"
+    van.id = RDF::URI.new "http://someiri.org/van"
     van.prefLabel = "van"
     van.synonym = ["cargo", "syn van"]
     van.definition = ["vehicle def 1", "vehicle def 2"]
-    van.parents = vehicle
+    van.parents = [vehicle]
     assert van.valid?
     van.save
-    assert_equal 1, count_pattern(
-      "GRAPH <#{submission.resource_id.value}> { #{van.resource_id.to_turtle} <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?p . }")
+
+    assert_equal 1, GooTest.count_pattern(
+      "GRAPH #{submission.id.to_ntriples} { #{van.id.to_ntriples} <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?p . }")
     cargo = Term.new
     cargo.submission = submission
-    cargo.resource_id = RDF::URI.new "http://someiri.org/cargo"
+    cargo.id = RDF::URI.new "http://someiri.org/cargo"
     cargo.prefLabel = "cargo"
     cargo.synonym = ["cargo yy", "cargo xx"]
     cargo.definition = ["cargo def 1", "cargo def 2"]
-    cargo.parents = vehicle
+    cargo.parents = [vehicle]
     assert cargo.valid?
     cargo.save
 
     minivan = Term.new
     minivan.submission = submission
-    minivan.resource_id = RDF::URI.new "http://someiri.org/minivan"
+    minivan.id = RDF::URI.new "http://someiri.org/minivan"
     minivan.prefLabel = "minivan"
     minivan.synonym = ["mini-van", "syn minivan"]
     minivan.definition = ["minivan def 1", "minivan def 2"]
-    minivan.parents = van
+    minivan.parents = [van]
     assert minivan.valid?
     minivan.save
 
 
     cargovan = Term.new
     cargovan.submission = submission
-    cargovan.resource_id = RDF::URI.new "http://someiri.org/cargovan"
+    cargovan.id = RDF::URI.new "http://someiri.org/cargovan"
     cargovan.prefLabel = "cargovan"
     cargovan.synonym = ["cargo van", "syn cargovan"]
     cargovan.definition = ["cargovan def 1", "cargovan def 2"]
@@ -232,55 +233,58 @@ class TestModelComplex < MiniTest::Unit::TestCase
     end
 
 
-    vehicle = Term.find(RDF::URI.new("http://someiri.org/vehicle"), submission: submission)
+    vehicle = Term.find(RDF::URI.new("http://someiri.org/vehicle")).in(submission)
+                .include(:children,:parents).first
     ch = vehicle.children
     assert ch.length == 2
-    (ch.select { |c| c.resource_id.value == "http://someiri.org/van" }).length == 1
-    (ch.select { |c| c.resource_id.value == "http://someiri.org/cargo" }).length == 1
-    assert vehicle.parents == []
+    (ch.select { |c| c.id.to_s == "http://someiri.org/van" }).length == 1
+    (ch.select { |c| c.id.to_s == "http://someiri.org/cargo" }).length == 1
+    assert vehicle.parents == nil
+
 
 
     assert cargovan.parents.length == 2
     #this is confussing
-    assert cargovan.children == []
+
+    Term.where.models([ cargovan ]).in(submission).include(:children).all
+    assert cargovan.children == nil
 
     #preload attrs
-    terms = Term.where submission: Submission.find("submission1"), load_attrs: [:parents, :synonym ]
+    terms = Term.in(Submission.find("submission1").first).include(:parents,:synonym,:definition)
     terms.each do |t|
-      if t.resource_id.value == "http://someiri.org/cargovan"
+      if t.id.to_s == "http://someiri.org/cargovan"
         assert_instance_of Array, t.parents
-        t.synonym.sort!
-        assert t.synonym.first.value == "cargo van"
-        assert t.synonym[1].value == "syn cargovan"
+        obj_sy = t.synonym.sort
+        assert obj_sy.first == "cargo van"
+        assert obj_sy[1] == "syn cargovan"
         assert (Set.new t.parents).length == t.parents.length
         assert t.parents.length == 2
         assert t.definition.length == 2
-        assert t.parents[0].kind_of? SparqlRd::Resultset::IRI
-        assert t.parents[1].kind_of? SparqlRd::Resultset::IRI
-        assert (t.parents.select { |x| x.value == "http://someiri.org/cargo" }).length == 1
-        assert (t.parents.select { |x| x.value == "http://someiri.org/van" }).length == 1
+        assert t.parents[0].kind_of?(Term)
+        assert t.parents[1].kind_of?(Term)
+        assert (t.parents.select { |x| x.id.to_s == "http://someiri.org/cargo" }).length == 1
+        assert (t.parents.select { |x| x.id.to_s == "http://someiri.org/van" }).length == 1
       end
-      if t.resource_id.value == "http://someiri.org/minivan"
+      if t.id.to_s == "http://someiri.org/minivan"
         assert_instance_of Array, t.parents
-        t.synonym.sort!
-        assert t.synonym.first.value == "mini-van"
-        assert t.synonym[1].value == "syn minivan"
+        obj_sy = t.synonym.sort
+        assert obj_sy.first == "mini-van"
+        assert obj_sy[1] == "syn minivan"
         assert t.parents.length == 1
-        assert t.parents[0].kind_of? SparqlRd::Resultset::IRI
-        assert t.parents[0].value == "http://someiri.org/van"
+        assert t.parents[0].kind_of?(Term)
+        assert t.parents[0].id.to_s == "http://someiri.org/van"
       end
-      if t.resource_id.value == "http://someiri.org/vehicle"
-        assert t.parents == []
+      if t.id.to_s == "http://someiri.org/vehicle"
+        assert t.parents == nil
       end
     end
     assert terms.length == 5
 
-    terms = Term.where submission: submission
+    terms = Term.in(submission)
     terms.each do |t|
       t.delete
-      assert_equal 0, count_pattern("GRAPH <#{submission.resource_id.value}> { #{t.resource_id.to_turtle} ?p ?o . }")
+      assert_equal 0, GooTest.count_pattern("GRAPH #{submission.id.to_ntriples} { #{t.id.to_ntriples} ?p ?o . }")
     end
-
     submission.delete
   end
 
