ba6d15cfb7db4aa8b988015abe36b91e69ecdc46
msalvadores <msalvadores@gmail.com>
2013-05-24 14:58:23 -0700

    append triples and post triples for sparql client

--- lib/goo/sparql/client.rb
+++ lib/goo/sparql/client.rb
@@ -10,9 +10,16 @@ module Goo
                                File.read(file_path) ,
                                :content_type => mime_type
       end
-      def append_triples(graph,file_path,mime_type=nil)
-        return RestClient.post "#{url.to_s}#{graph.to_s}",
-                               File.read(file_path) ,
+      def append_triples(graph,data,mime_type=nil)
+        return RestClient.post "#{url.to_s}",
+                               :graph => graph.to_s,
+                               :data => data ,
+                               :content_type => mime_type
+      end
+      def append_triples_from_file(graph,file_path,mime_type=nil)
+        return RestClient.post "#{url.to_s}",
+                               :graph => graph.to_s,
+                               :data => File.read(file_path) ,
                                :content_type => mime_type
       end
       def delete_graph(graph)
