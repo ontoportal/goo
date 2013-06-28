b5dcc7ed87672a23c08396b2a5be26a9194bf165
Paul R Alexander <palexander@stanford.edu>
2013-06-28 16:55:27 -0700

    Add validation for attributes via proc

--- lib/goo/validators/enforce.rb
+++ lib/goo/validators/enforce.rb
@@ -10,11 +10,11 @@ module Goo
       def self.enforce_type_boolean(attr,value)
         if value.kind_of? Array
           if (value.select {|x| !((x.class == TrueClass) || (x.class == FalseClass))} ).length > 0
-            return  "All values in attribute `#{attr}` must be `Boolean`" 
+            return  "All values in attribute `#{attr}` must be `Boolean`"
           end
         else
           if !((value.class == TrueClass) || (value.class == FalseClass))
-            return  "Attribute `#{attr}` value `#{value}` must be a `Boolean`" 
+            return  "Attribute `#{attr}` value `#{value}` must be a `Boolean`"
           end
         end
       end
@@ -25,11 +25,11 @@ module Goo
         end
         if value.kind_of? Array
           if (value.select {|x| !(x.kind_of? type)} ).length > 0
-            return  "All values in attribute `#{attr}` must be `#{type.name}`" 
+            return  "All values in attribute `#{attr}` must be `#{type.name}`"
           end
         else
           if !(value.kind_of? type)
-            return  "Attribute `#{attr}` value `#{value}` must be a `#{type.name}`" 
+            return  "Attribute `#{attr}` value `#{value}` must be a `#{type.name}`"
           end
         end
       end
@@ -55,61 +55,64 @@ module Goo
         return nil if enforce_opts.nil? or enforce_opts.length == 0
         errors_by_opt = {}
         enforce_opts.each do |opt|
-          case opt 
-           when :unique
-             unless value.nil?
-               dup = Goo::SPARQL::Queries.duplicate_attribute_value?(inst,attr)
-               if dup
-                 add_error(opt, errors_by_opt, 
-                 "`#{attr}` must be unique. " +
-                 "There are other model instances with the same attribute value `#{value}`.")
-               end
-             end
-           when :no_list
-             if value.kind_of? Array
-               add_error(opt, errors_by_opt, 
-                       "`#{attr}` is defined as non Array - it cannot hold multiple values") 
-             end
-           when :existence
-             add_error(opt, errors_by_opt, "`#{attr}` value cannot be nil") if value.nil?
-           when :list, Array
-             if !value.nil? && !(value.kind_of? Array)
-               add_error(opt, errors_by_opt, "`#{attr}` value must be an Array") 
-             end
-           when :uri, RDF::URI
-             add_error(opt, errors_by_opt, enforce_type(attr,RDF::URI,value)) unless value.nil?
-           when :string, String
-             add_error(opt, errors_by_opt, enforce_type(attr,String,value)) unless value.nil?
-           when :integer, Fixnum
-             add_error(opt, errors_by_opt, enforce_type(attr,Fixnum,value)) unless value.nil?
-           when :boolean
-             add_error(opt, errors_by_opt, enforce_type(attr,:boolean,value)) unless value.nil?
-           when :date_time, DateTime
-             add_error(opt, errors_by_opt, enforce_type(attr,DateTime,value)) unless value.nil?
-           else
-             model_range = opt.respond_to?(:shape_attribute) ? opt : Goo.model_by_name(opt)
-             if model_range and !value.nil?
-               values = value.kind_of?(Array) ? value : [value]
-               values.each do |v|
-                 if not v.kind_of?(model_range)
-                   add_error(model_range.model_name, errors_by_opt,
-                             "`#{attr}` contains values that are not instance of `#{model_range.model_name}`")
-                 else
-                   if not v.persistent?
-                     add_error(model_range.model_name, errors_by_opt,
-                               "`#{attr}` contains non persistent models. It will not save.")
-                   end
-                 end
-               end
-             end
-             opt_s = opt.to_s
-             if opt_s.index("max_") == 0
-               add_error(:max, errors_by_opt, enforce_range_length(:max,attr,opt_s,value)) unless value.nil?
-             end
-             if opt_s.index("min_") == 0
-               add_error(:min, errors_by_opt, enforce_range_length(:min,attr,opt_s,value)) unless value.nil?
-             end
-           end
+          case opt
+          when :unique
+            unless value.nil?
+              dup = Goo::SPARQL::Queries.duplicate_attribute_value?(inst,attr)
+              if dup
+                add_error(opt, errors_by_opt,
+                "`#{attr}` must be unique. " +
+                "There are other model instances with the same attribute value `#{value}`.")
+              end
+            end
+          when :no_list
+            if value.kind_of? Array
+              add_error(opt, errors_by_opt,
+                      "`#{attr}` is defined as non Array - it cannot hold multiple values")
+            end
+          when :existence
+            add_error(opt, errors_by_opt, "`#{attr}` value cannot be nil") if value.nil?
+          when :list, Array
+            if !value.nil? && !(value.kind_of? Array)
+              add_error(opt, errors_by_opt, "`#{attr}` value must be an Array")
+            end
+          when :uri, RDF::URI
+            add_error(opt, errors_by_opt, enforce_type(attr,RDF::URI,value)) unless value.nil?
+          when :string, String
+            add_error(opt, errors_by_opt, enforce_type(attr,String,value)) unless value.nil?
+          when :integer, Fixnum
+            add_error(opt, errors_by_opt, enforce_type(attr,Fixnum,value)) unless value.nil?
+          when :boolean
+            add_error(opt, errors_by_opt, enforce_type(attr,:boolean,value)) unless value.nil?
+          when :date_time, DateTime
+            add_error(opt, errors_by_opt, enforce_type(attr,DateTime,value)) unless value.nil?
+          when Proc
+            validator_name, error_message = opt.call(inst, attr)
+            add_error(validator_name.to_sym, errors_by_opt, error_message) unless error_message.nil?
+          else
+            model_range = opt.respond_to?(:shape_attribute) ? opt : Goo.model_by_name(opt)
+            if model_range and !value.nil?
+              values = value.kind_of?(Array) ? value : [value]
+              values.each do |v|
+                if not v.kind_of?(model_range)
+                  add_error(model_range.model_name, errors_by_opt,
+                            "`#{attr}` contains values that are not instance of `#{model_range.model_name}`")
+                else
+                  if not v.persistent?
+                    add_error(model_range.model_name, errors_by_opt,
+                              "`#{attr}` contains non persistent models. It will not save.")
+                  end
+                end
+              end
+            end
+            opt_s = opt.to_s
+            if opt_s.index("max_") == 0
+              add_error(:max, errors_by_opt, enforce_range_length(:max,attr,opt_s,value)) unless value.nil?
+            end
+            if opt_s.index("min_") == 0
+              add_error(:min, errors_by_opt, enforce_range_length(:min,attr,opt_s,value)) unless value.nil?
+            end
+          end
         end
         return errors_by_opt.length > 0 ? errors_by_opt : nil
       end
