6fb2d1979977ccfc3d7974e15e35ac3b88bb5415
msalvadores <msalvadores@gmail.com>
2013-07-29 13:18:15 -0700

    test for dependent objects and ready - one attr

--- test/test_model_complex.rb
+++ test/test_model_complex.rb
@@ -550,6 +550,37 @@ class TestModelComplex < MiniTest::Unit::TestCase
     assert page_terms.length == 0
   end
 
+  def test_readonly_pages_with_include
+    submission = Submission.new(name: "submission1")
+    unless submission.exist?
+      submission.save
+    else
+      submission = Submission.find("submission1").first
+    end
+    terms = Term.in(submission)
+    terms.each do |t|
+      t.delete
+      assert_equal(0,
+       GooTest.count_pattern("GRAPH #{submission.id.to_ntriples} { #{t.id.to_ntriples} ?p ?o . }"))
+    end
+    terms = []
+    10.times do |i|
+      term = Term.new
+      term.id = RDF::URI.new("http://someiri.org/term/#{i}")
+      term.submission = submission
+      term.prefLabel = "term #{i}"
+      if i >= 1 && i < 5
+        term.parents = [terms[0]]
+      elsif i >= 2
+        term.parents = [terms[1]]
+      end
+      assert term.valid?
+      term.save
+      terms << term
+    end
+
+  end
+
   def test_nil_attributes
     t = Term.new
     assert_raises NoMethodError do
