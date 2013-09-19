eb4ba6f43126ec333cba40ab9624137cdc32a0a2
msalvadores <msalvadores@gmail.com>
2013-09-19 08:19:54 -0700

    no need for this hacky filters any more

--- lib/goo/sparql/queries.rb
+++ lib/goo/sparql/queries.rb
@@ -481,13 +481,6 @@ module Goo
 
         select.filter(filter_id_str)
 
-        #these filters are only needed in class LD model
-        #TODO: move this if to confiration DSL option
-        if klass.model_name == :class
-          select.filter("!isBLANK(?id)")
-          select.filter("!isBLANK(?object)") if unmapped
-        end
-
         #if unmapped && predicates && predicates.length > 0
         #  filter_predicates = predicates.map { |p| "?predicate = #{p.to_ntriples}" }
         #  filter_predicates = filter_predicates.join " || "
