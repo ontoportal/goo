39de7ffdc0b3dab3795ccdd51ac007ab53cbdd6a
mdorf <mdorf@stanford.edu>
2017-03-03 17:59:35 -0800

    implemented the ability to have multiple search backends - for ncbo/ontologies_linked_data#69

--- lib/goo/search/search.rb
+++ lib/goo/search/search.rb
@@ -8,28 +8,28 @@ module Goo
         base.extend(ClassMethods)
       end
 
-      def index()
+      def index(connection_name=:main)
         raise ArgumentError, "ID must be set to be able to index" if @id.nil?
-        doc = get_indexable_object()
+        doc = get_indexable_object
         #solr wants resource_id instead of :id
-        Goo.search_connection.add(doc)
+        Goo.search_connection(connection_name).add(doc)
       end
 
-      def unindex()
-        id = get_index_id()
-        Goo.search_connection.delete_by_id(id)
+      def unindex(connection_name=:main)
+        id = get_index_id
+        Goo.search_connection(connection_name).delete_by_id(id)
       end
 
       def get_index_id()
-        return self.class.model_settings[:search_options][:index_id].call(self)
+        self.class.model_settings[:search_options][:index_id].call(self)
       end
 
       def get_indexable_object()
         doc = self.class.model_settings[:search_options][:document].call(self)
         #in solr
         doc[:resource_id] = doc[:id].to_s
-        doc[:id] = get_index_id().to_s
-        return doc
+        doc[:id] = get_index_id.to_s
+        doc
       end
 
       module ClassMethods
@@ -37,46 +37,45 @@ module Goo
         def search_options(*args)
           @model_settings[:search_options] = args.first
         end
-        
-        def search(q, params={})
+
+        def search(q, params={}, connection_name=:main)
           params["q"] = q
-          response = Goo.search_connection.post('select', :data => params)
-          return response
+          Goo.search_connection(connection_name).post('select', :data => params)
         end
 
-        def indexBatch(collection)
+        def indexBatch(collection, connection_name=:main)
           docs = Array.new
           collection.each do |c|
-            docs << c.get_indexable_object()
+            docs << c.get_indexable_object
           end
 
-          Goo.search_connection.add(docs)
+          Goo.search_connection(connection_name).add(docs)
         end
 
-        def unindexBatch(collection)
+        def unindexBatch(collection, connection_name=:main)
           docs = Array.new
           collection.each do |c|
-            docs << c.get_index_id()
+            docs << c.get_index_id
           end
 
-          Goo.search_connection.delete_by_id(docs)
+          Goo.search_connection(connection_name).delete_by_id(docs)
         end
 
-        def unindexByQuery(query)
-          Goo.search_connection.delete_by_query(query)
+        def unindexByQuery(query, connection_name=:main)
+          Goo.search_connection(connection_name).delete_by_query(query)
         end
 
-        def indexCommit(attrs=nil)
-          Goo.search_connection.commit(:commit_attributes => attrs || {})
+        def indexCommit(attrs=nil, connection_name=:main)
+          Goo.search_connection(connection_name).commit(:commit_attributes => attrs || {})
         end
 
-        def indexOptimize(attrs=nil)
-          Goo.search_connection.optimize(:optimize_attributes => attrs || {})
+        def indexOptimize(attrs=nil, connection_name=:main)
+          Goo.search_connection(connection_name).optimize(:optimize_attributes => attrs || {})
         end
 
-        def indexClear()
+        def indexClear(connection_name=:main)
           # WARNING: this deletes ALL data from the index
-          unindexByQuery("*:*")
+          unindexByQuery("*:*", connection_name)
         end
       end
   end
