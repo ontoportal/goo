46d1b9f7be2a9fc1c9857cdca099359c18878820
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-02-28 03:25:28 +0100

    update existence validator to not accept empty to_s  objects

--- lib/goo/validators/implementations/existence.rb
+++ lib/goo/validators/implementations/existence.rb
@@ -8,15 +8,22 @@ module Goo
       error_message ->(obj) { "`#{@value}` value cannot be nil"}
 
       validity_check -> (obj) do
-        not (@value.nil? || self.class.empty_string?(@value) || self.class.empty_array?(@value))
+        not (@value.nil? || self.class.empty?(@value) || self.class.empty_array?(@value))
       end
 
+      def self.empty?(value)
+        empty_string?(value) || empty_to_s?(value)
+      end
       def self.empty_string?(string)
         string.is_a?(String) && string.strip.empty?
       end
 
+      def self.empty_to_s?(object)
+        object && object.to_s&.strip.empty?
+      end
+
       def self.empty_array?(array)
-        array.is_a?(Array) && array && array.reject{|x|  x.nil? || empty_string?(x)}.empty?
+        array.is_a?(Array) && array && array.reject{|x|  x.nil? || empty?(x)}.empty?
       end
     end
   end
