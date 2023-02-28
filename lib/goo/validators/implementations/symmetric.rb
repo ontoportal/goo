9e4739c8cd177367df4d7e02d373a956db780e00
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-02-28 08:33:19 +0100

    update symmetric code and error message

--- lib/goo/validators/implementations/symmetric.rb
+++ lib/goo/validators/implementations/symmetric.rb
@@ -6,39 +6,28 @@ module Goo
       key :symmetric
 
       error_message ->(obj) {
-        "symmetric error"
+        "`#{@attr}` must be symmetric"
       }
 
       validity_check -> (obj) do
-        return true if Existence.empty_value?(@value)
+        return true if self.class.empty_value?(@value)
 
-        return Array(@value).select{|x| not self.class.symmetric?(@attr,x, @inst)}.empty?
+        return Array(@value).select{|x| not symmetric?(@attr,x, @inst)}.empty?
       end
 
-      def self.symmetric?(attr, value, source_object)
-        if self.respond_to?(attr, value)
-          target_values = self.attr_value(attr, value)
-          return target_values.any?{ |target_object| self.equivalent?(target_object, source_object)}
+      def symmetric?(attr, value, source_object)
+        if respond_to?(attr, value)
+          target_values = self.class.attr_value(attr, value)
+          return target_values.any?{ |target_object| self.class.equivalent_value?(target_object, source_object)}
         end
 
         return false
       end
 
-      def self.respond_to?(attr, object)
+      def respond_to?(attr, object)
         object && object.respond_to?(attr)
       end
 
-      def self.attr_value(attr, object)
-        Array(object.send(attr))
-      end
-
-      def self.equivalent?(object1, object2)
-        if object1.respond_to?(:id) && object2.respond_to?(:id)
-          object1.id.eql?(object2.id)
-        else
-          object2 == object1
-        end
-      end
     end
   end
 end
